f4c0773295f3d77cf8be042f422241d19a87a668
nananiev <nananiev@cleancodefactory.de>
2020-02-06 11:38:29 +0200

    Secret manager added

--- TopkaE.FPLDataDownloader/Startup.cs
+++ TopkaE.FPLDataDownloader/Startup.cs
@@ -30,9 +30,9 @@ namespace TopkaE.FPLDataDownloader
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            string connectionString = Configuration["MariaDBConnection"];
             services.AddDbContext<TopkaEContext>(options => options.
-                //UseSqlite("Data Source=" + Environment.CurrentDirectory + "\\Database\\fpldata.db").
-                UseMySql(Configuration.GetConnectionString("MariaDBConnection"))
+                UseMySql(connectionString)
             );
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
